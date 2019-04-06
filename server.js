a8d8423a067f7cc44c642bf72f3fcc4a5e5785c3
Volodymyr Kovalenko <vokovalenko16@gmail.com>
2019-04-06 11:17:38 -0700

    working

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
 var express = require("express");
 var exphbs = require("express-handlebars");
-
 var db = require("./models");
+var pricesJs = require("./prices")
+
+
+pricesJs();
 
 var app = express();
 var PORT = process.env.PORT || 3000;
